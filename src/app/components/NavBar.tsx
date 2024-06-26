df333ec19222bf9d5750ed52c20199735320c22b
Kiara Mutschler <43933594+kjmutsch@users.noreply.github.com>
2024-06-25 22:11:12 -0500

    get svg in app

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+        <button>
+            About
+        </button>
+        <button>
+            Mission
+        </button>
+        <button>
+            Resume
+        </button>
+        <button>
+            Projects
+        </button>
+        <button>
+            Contact
+        </button>
+
+    </div>
+  );
+}
