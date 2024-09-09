c38a385c0dbc8fcf4dd0e488b873c015b6d65ea7
Kiara Mutschler <43933594+kjmutsch@users.noreply.github.com>
2024-09-09 13:51:10 -0500

    add images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html  className="h-screen w-screen" lang="en">
+      <body className="h-screen w-screen">{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,32 @@ import Background from "./components/Background_Final";
 import Start from "./components/Start"
 import AudioPlayer from "./components/AudioPlayer";
 import useSound from 'use-sound'
+import { Button } from "@/components/ui/button"
+import { renderToStaticMarkup } from 'react-dom/server';
 
 export default function Home() {
   const [playSubmitSound, setPlaySubmitSound] = useState(false);
   const [play] = useSound('/LatinHouseBed.mp3')
-  return (
-    <>
-      <AudioPlayer onFinish={() => setPlaySubmitSound(false)} play={true} src="/LatinHouseBed.mp3" />
-        <Start />
-        <Background />
-        <button 
-          onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => play()}
-      >sound</button>
-    </>
+  const [onStart, setOnStart] = useState(true);
 
+  function startButtonClick() {
+    play();
+    setOnStart(false);
+  }
+
+  return (
+    // <h1 className="flex-1 min-h-screen w-screen bg-no-repeat"  style={{
+    //     backgroundImage: `url("data:image/svg+xml,${svgString}")`
+    //   }}>
+    <div className="h-screen w-screen">
+      <Background />
+      <AudioPlayer onFinish={() => setPlaySubmitSound(false)} play={true} src="/LatinHouseBed.mp3" />
+      <div className="flex-1 size-full">
+        <button className="flex-1 size-full" onClick={startButtonClick}>
+          <Start />
+        </button>
+      </div>
+     {/* </h1> */}
+        </div>
   );
 }
